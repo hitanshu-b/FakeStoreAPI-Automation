81d5194d4f0285246106b4f45c22ac6af6c9c0d9
Hitanshu Bagde <hitanshubagde196@gmail.com>
2024-12-30 19:13:10 +0530

    Initial Commit

--- src/test/java/org/Tests/ProductTest.java
+++ src/test/java/org/Tests/ProductTest.java
@@ -5,6 +5,7 @@ import org.Actions.AssertActions;
 import org.Base.CommonFunctions;
 import org.Endpoints.APIConstants;
 import org.Modules.PayloadManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ProductTest {
@@ -26,5 +27,51 @@ public class ProductTest {
                 .post(APIConstants.CREATE_PRODUCT);
         assertions.assertStatusCode(response, 201);
         assertions.assertResponseField(response, "title", "Sample Product");
+    }
+
+    @Test
+    public void testCreateProductWithMissingTitle(){
+        String payload = PayloadManager.getProductPayload(3);
+        Response response = CommonFunctions.getRequestSpecification()
+                .body(payload)
+                .post(APIConstants.CREATE_PRODUCT);
+        assertions.assertStatusCode(response,400);
+    }
+
+    @Test
+    public void testCreateProductWithInvalidPrice(){
+        String payload = PayloadManager.getProductPayload(4);
+        Response response = CommonFunctions.getRequestSpecification()
+                .body(payload)
+                .post(APIConstants.CREATE_PRODUCT);
+        assertions.assertStatusCode(response,400);
+    }
+
+    @Test
+    public void testCreateProductWithMissingDetails(){
+        String payload = PayloadManager.getProductPayload(7);
+        Response response = CommonFunctions.getRequestSpecification()
+                .body(payload)
+                .post(APIConstants.CREATE_PRODUCT);
+        assertions.assertStatusCode(response,400);
+    }
+
+    @Test
+    public void testUpdateProduct(){
+        String payload = PayloadManager.getProductPayload(1);
+        int productId = 2;
+        Response response = CommonFunctions.getRequestSpecification()
+                .pathParam("id",productId)
+                .body(payload)
+                .put(APIConstants.UPDATE_PRODUCT);
+
+        assertions.assertStatusCode(response,200);
+        assertions.assertResponseField(response,"title","Premium Laptop");
+//        assertions.assertResponseField(response,"price",1299.99);
+        double actualPrice = response.jsonPath().getDouble("price");
+        double expectedPrice = 1299.99;
+        boolean isPriceValid = (actualPrice >= expectedPrice - 0.01) && (actualPrice <= expectedPrice + 0.01);
+
+        Assert.assertTrue(isPriceValid, "Expected Price:"+ expectedPrice + "Actual Price:" + actualPrice);
     }
 }
